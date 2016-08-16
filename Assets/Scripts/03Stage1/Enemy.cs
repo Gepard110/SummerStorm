8d4c19ba5000a08f10448dda3ab394f008d019d8
BuildTools <hiro-spica830@i.softbank.jp>
2016-08-16 12:16:15 +0900

    敵の動きを完成させました

---
+++ Assets/Scripts/03Stage1/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+	public GameObject EnemyFire;
+	public bool canfire = false;
+	 public float angle;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player") {
+			//transform.LookAt(other.transform);
+			StartCoroutine ("hogehoge");
+			this.gameObject.transform.LookAt (other.transform.position);
+			transform.Rotate (new Vector3 (0f, 90f, 0f));  
+		}
+	}
+
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if (other.tag == "Player") {
+			canfire = true;
+			transform.position -= transform.right * 0.05f;
+			Vector3 distance = other.gameObject.transform.position-this.transform.position;
+			float value = distance.y / distance.x ;
+			float theta = Mathf.Atan(value);
+			if(this.transform.position.x > other.gameObject.transform.position.x){
+				angle = theta * Mathf.Rad2Deg;
+			}else{
+				angle = 180 + theta * Mathf.Rad2Deg;
+			}
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other){
+		canfire = false;
+	}
+
+	IEnumerator hogehoge ()
+	{
+		yield return new WaitForSeconds (1);
+		if (canfire == true) {
+			Instantiate (EnemyFire, this.transform.position, Quaternion.Euler (angle, 270,  0));
+			StartCoroutine ("hogehoge");
+		}
+	}
+}
---
+++ Assets/Scripts/03Stage1/fire.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fire : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("hogehoge");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * 0.07f;
+	}
+
+	IEnumerator hogehoge(){
+		yield return new WaitForSeconds(2.5f);
+		Destroy(this.gameObject);
+	}
+}
